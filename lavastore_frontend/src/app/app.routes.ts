892b153a931f6e548ee6303e87104cb203633dd7
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-05-09 01:03:23 +0300

    Add initial products home component with routing and layout integration

--- lavastore_frontend/src/app/app.routes.ts
+++ lavastore_frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { BaseComponent } from './layouts/base/base.component';
 import { AuthComponent } from './layouts/auth/auth.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
+import { ProductsHomeComponent } from './products-home/products-home.component';
 
 export const routes: Routes = [
     { path: '', component: BaseComponent },
     { path: 'auth/signup', component: SignupComponent },
     { path: 'auth/signin', component: SigninComponent },
+    { path: 'products', component: ProductsHomeComponent },
 ];
---
+++ lavastore_frontend/src/app/products-home/products-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "../layouts/home/home.component";
+
+@Component({
+  selector: 'app-products-home',
+  imports: [HomeComponent],
+  templateUrl: './products-home.component.html',
+  styleUrl: './products-home.component.css'
+})
+export class ProductsHomeComponent {
+
+}
