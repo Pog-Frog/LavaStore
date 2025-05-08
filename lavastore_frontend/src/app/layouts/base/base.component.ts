51f47690ccc8fddf26d6ef468311b3b0676f2d46
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-05-08 20:58:08 +0300

    Add home page

--- lavastore_frontend/src/app/layouts/base/base.component.ts
+++ lavastore_frontend/src/app/layouts/base/base.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import { BannerComponent } from '../../banner/banner.component';
+import { HomeContentComponent } from '../../home-content/home-content.component';
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-base',
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, HomeContentComponent, FooterComponent],
   templateUrl: './base.component.html',
   styleUrl: './base.component.css'
 })
