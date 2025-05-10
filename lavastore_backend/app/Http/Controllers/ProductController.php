3fb4bdf0c7f29a5c378d915a0107b45dfa74b0af
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-05-10 22:23:13 +0300

    Implement Product CRUD operations in ProductController and update routing configuration

--- lavastore_backend/app/Http/Controllers/ProductController.php
+++ lavastore_backend/app/Http/Controllers/ProductController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductCreateRequest;
+use App\Http\Requests\ProductUpdateRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Http\Repositories\ProductRepository;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class ProductController extends Controller
 {
-    public function index(Request $request)
+    use AuthorizesRequests;
+    
+    public function __construct(protected ProductRepository $productRepository)
+    {
+        $this->authorizeResource(Product::class, 'product');
+    }
+
+    public function index(Request $request): JsonResponse
     {
         $products = QueryBuilder::for(Product::class)
             ->allowedFilters(Product::allowedFilters())
@@ -17,6 +29,54 @@ class ProductController extends Controller
             ->paginate()
             ->appends($request->query());
 
-        return response()->json($products);
+        return response()->json([
+            'message' => 'Products retrieved successfully',
+            'data' => $products
+        ], 200);
     }
-} 
+
+    public function store(ProductCreateRequest $request): JsonResponse
+    {
+        $product = $this->productRepository->create($request->validated());
+
+        return response()->json([
+            'message' => 'Product created successfully',
+            'data' => $product
+        ], 201);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $product = $this->productRepository->getById($id);
+
+        if($product == null) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Product retrieved successfully',
+            'data' => $product
+        ], 200);
+    }
+
+    public function update(ProductUpdateRequest $request, string $id): JsonResponse
+    {
+        $product = $this->productRepository->update($id, $request->validated());
+
+        return response()->json([
+            'message' => 'Product updated successfully',
+            'data' => $product
+        ]);
+    }
+
+    public function destroy(string $id): JsonResponse
+    {
+        $this->productRepository->delete($id);
+
+        return response()->json([
+            'message' => 'Product deleted successfully'
+        ]);
+    }
+}
--- lavastore_backend/app/Http/Repositories/ProductRepository.php
+++ lavastore_backend/app/Http/Repositories/ProductRepository.php
@@ -7,7 +7,11 @@ use App\Models\Product;
 class ProductRepository {
 
     public function getById(string $id) {
-        return Product::findOrFail($id)->with('dietaryPreferences');
+        $product = Product::query()->where('id', $id)->with('dietaryPreferences')->first();
+        if(!$product) {
+            return null;
+        }
+        return $product;
     }
 
     public function getAll() {
@@ -23,13 +27,21 @@ class ProductRepository {
         return $product;
     }
 
-    public function update(Product $product, array $data) {
+    public function update(string $id, array $data) {
+        $product = Product::find($id);
+        if(!$product) {
+            return null;
+        }
         $product->update($data);
         $product->dietaryPreferences()->sync($data['dietary_preferences']);
         return $product;
     }   
 
-    public function delete(Product $product) {
+    public function delete(string $id) {
+        $product = Product::find($id);
+        if(!$product) {
+            return null;
+        }
         $product->dietaryPreferences()->detach();
         return $product->delete();
     }
