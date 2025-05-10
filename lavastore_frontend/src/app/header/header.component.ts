ac840590bcc8330a3ee5566baff12a84825eca9e
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-05-11 01:16:16 +0300

    Made the header more responsive

--- lavastore_frontend/src/app/header/header.component.ts
+++ lavastore_frontend/src/app/header/header.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMobileMenuOpen = false;
+  cartItemsCount = 0;
 
+  ngOnInit(): void {
+    
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
 }
