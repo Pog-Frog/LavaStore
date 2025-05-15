631f0e116fa926c6dba3cc87e4565ab4a3b7b156
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-05-15 12:55:04 +0300

    Reorganize API routes to group authenticated endpoints

--- lavastore_backend/routes/api.php
+++ lavastore_backend/routes/api.php
@@ -7,24 +7,24 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/featured', [ProductController::class, 'getFeaturedProducts']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::get('/dietary-preferences', [DietaryPreferenceController::class, 'index']);
-Route::get('/dietary-preferences/{id}', [DietaryPreferenceController::class, 'show']);
 
+Route::get('/products/featured', [ProductController::class, 'getFeaturedProducts']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
     Route::post('products', [ProductController::class, 'store']);
     Route::put('products/{id}', [ProductController::class, 'update']);
     Route::delete('products/{id}', [ProductController::class, 'destroy']);
 
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::post('categories', [CategoryController::class, 'store']);
     Route::put('categories/{id}', [CategoryController::class, 'update']);
     Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
 
+    Route::get('/dietary-preferences', [DietaryPreferenceController::class, 'index']);
+    Route::get('/dietary-preferences/{id}', [DietaryPreferenceController::class, 'show']);
     Route::post('dietary-preferences', [DietaryPreferenceController::class, 'store']);
     Route::put('dietary-preferences/{id}', [DietaryPreferenceController::class, 'update']);
     Route::delete('dietary-preferences/{id}', [DietaryPreferenceController::class, 'destroy']);
